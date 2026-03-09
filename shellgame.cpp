29c24f11ccbd0f5b7423badd6ddf0f030ed748de
Bronson <bronson@2603-9000-7800-31b3-0000-0000-0000-1816.inf6.spectrum.com>
2026-03-09 12:54:48 -0400

    shell game problem

--- shellgame.cpp
+++ shellgame.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using vi = vector<int>;
 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  if (!(cin >> n))
+    return 0;
+
+  vi shell = {0, 1, 2, 3};
+  vi point(4, 0);
+
+  for (int i = 0; i < n; i++) {
+    int a, b, g;
+    cin >> a >> b >> g;
+
+    swap(shell[a], shell[b]);
+    int correct = shell[g];
+    point[correct]++;
+  }
+
+  cout << max({point[1], point[2], point[3]}) << endl;
+
+  return 0;
+}
